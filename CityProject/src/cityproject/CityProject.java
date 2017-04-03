0ea2017b58c29fd81d9457ec244888120f409054
Dixi <dixilindez@gmail.com>
2017-04-03 19:35:12 +0200

    [Cours 6] Ajout d'un enum, complement sur notre projet

--- CityProject/src/cityproject/CityProject.java
+++ CityProject/src/cityproject/CityProject.java
@@ -4,8 +4,11 @@
 package cityproject;
 
 import cityproject.infrastructures.Quartier;
+import cityproject.infrastructures.Rue;
 import cityproject.infrastructures.Ville;
+import cityproject.infrastructures.geographie.Coordonnees4Points;
 import cityproject.infrastructures.geographie.CoordonneesGeographiques;
+import cityproject.infrastructures.geographie.DirectionEnum;
 import cityproject.infrastructures.geographie.PointKm;
 import cityproject.infrastructures.geographie.PointM;
 
@@ -22,49 +25,60 @@ public class CityProject {
 	 */
 	public static void main(String[] args) throws Exception {
 		// Creation de la ville
-		Ville ville = new Ville("CityProject ville");
-		CoordonneesGeographiques<PointKm> coordVille = new CoordonneesGeographiques<PointKm>();
-		coordVille.ajouterElement(new PointKm(1, 30));
-		coordVille.ajouterElement(new PointKm(5, 50));
-		coordVille.ajouterElement(new PointKm(10, 55));
-		coordVille.ajouterElement(new PointKm(15, 70));
-		coordVille.ajouterElement(new PointKm(25, 72));
-		coordVille.ajouterElement(new PointKm(35, 65));
-		coordVille.ajouterElement(new PointKm(65, 68));
-		coordVille.ajouterElement(new PointKm(100, 65));
-		coordVille.ajouterElement(new PointKm(110, 50));
-		coordVille.ajouterElement(new PointKm(115, 25));
-		coordVille.ajouterElement(new PointKm(110, 3));
-		coordVille.ajouterElement(new PointKm(100, 1));
-		coordVille.ajouterElement(new PointKm(90, 15));
-		coordVille.ajouterElement(new PointKm(75, 20));
-		coordVille.ajouterElement(new PointKm(60, 25));
-		coordVille.ajouterElement(new PointKm(45, 10));
-		coordVille.ajouterElement(new PointKm(30, 5));
-		coordVille.ajouterElement(new PointKm(15, 1));
-		coordVille.ajouterElement(new PointKm(4, 3));
-		ville.setCoordonnees(coordVille);
+//		Ville ville = new Ville("CityProject ville");
+//		CoordonneesGeographiques<PointKm> coordVille = new CoordonneesGeographiques<PointKm>();
+//		coordVille.ajouterElement(new PointKm(1, 30));
+//		coordVille.ajouterElement(new PointKm(5, 50));
+//		coordVille.ajouterElement(new PointKm(10, 55));
+//		coordVille.ajouterElement(new PointKm(15, 70));
+//		coordVille.ajouterElement(new PointKm(25, 72));
+//		coordVille.ajouterElement(new PointKm(35, 65));
+//		coordVille.ajouterElement(new PointKm(65, 68));
+//		coordVille.ajouterElement(new PointKm(100, 65));
+//		coordVille.ajouterElement(new PointKm(110, 50));
+//		coordVille.ajouterElement(new PointKm(115, 25));
+//		coordVille.ajouterElement(new PointKm(110, 3));
+//		coordVille.ajouterElement(new PointKm(100, 1));
+//		coordVille.ajouterElement(new PointKm(90, 15));
+//		coordVille.ajouterElement(new PointKm(75, 20));
+//		coordVille.ajouterElement(new PointKm(60, 25));
+//		coordVille.ajouterElement(new PointKm(45, 10));
+//		coordVille.ajouterElement(new PointKm(30, 5));
+//		coordVille.ajouterElement(new PointKm(15, 1));
+//		coordVille.ajouterElement(new PointKm(4, 3));
+//		ville.setCoordonnees(coordVille);
+//		
+//		
+//		Quartier quartier = new Quartier("Le quartier centrale");
+//		CoordonneesGeographiques<PointKm> coordQuartier = new CoordonneesGeographiques<PointKm>();
+//		coordQuartier.ajouterElement(new PointKm(10, 30));
+//		coordQuartier.ajouterElement(new PointKm(10, 35));
+//		coordQuartier.ajouterElement(new PointKm(20, 37));
+//		coordQuartier.ajouterElement(new PointKm(24, 29));
+//		coordQuartier.ajouterElement(new PointKm(25, 25));
+//		coordQuartier.ajouterElement(new PointKm(30, 25));
+//		coordQuartier.ajouterElement(new PointKm(30, 20));
+//		coordQuartier.ajouterElement(new PointKm(35, 20));
+//		coordQuartier.ajouterElement(new PointKm(30, 25));
+//		coordQuartier.ajouterElement(new PointKm(20, 20));
+//				
+//		quartier.setCoordonnees(coordQuartier);
+//		quartier.setVille(ville);
+//		System.out.println(quartier);
+		
+		System.out.println(DirectionEnum.HORIZONTAL);
 		
 		
-		Quartier quartier = new Quartier("Le quartier centrale");
-		CoordonneesGeographiques<PointKm> coordQuartier = new CoordonneesGeographiques<PointKm>();
-		coordQuartier.ajouterElement(new PointKm(10, 30));
-		coordQuartier.ajouterElement(new PointKm(10, 35));
-		coordQuartier.ajouterElement(new PointKm(20, 37));
-		coordQuartier.ajouterElement(new PointKm(24, 29));
-		coordQuartier.ajouterElement(new PointKm(25, 25));
-		coordQuartier.ajouterElement(new PointKm(30, 25));
-		coordQuartier.ajouterElement(new PointKm(30, 20));
-		coordQuartier.ajouterElement(new PointKm(35, 20));
-		coordQuartier.ajouterElement(new PointKm(30, 25));
-		coordQuartier.ajouterElement(new PointKm(20, 20));
-				
-		quartier.setCoordonnees(coordQuartier);
-		quartier.setVille(ville);
-		System.out.println(quartier);
+		Rue rue = new Rue("Ma belle rue");
+		Coordonnees4Points<PointM> coordRue = new Coordonnees4Points<PointM>();
+		coordRue.ajouterElement(new PointM(5, 5));
+		coordRue.ajouterElement(new PointM(15, 5));
+		coordRue.ajouterElement(new PointM(5, 15));
+		coordRue.ajouterElement(new PointM(10, 5));
 		
-		String t = null;
-		t.toString();
+		if (coordRue.isValid()) {
+			rue.setCoordonnees(coordRue);
+		}
 	}
 
 }
--- CityProject/src/cityproject/infrastructures/Rue.java
+++ CityProject/src/cityproject/infrastructures/Rue.java
@@ -1,9 +1,44 @@
 package cityproject.infrastructures;
 
-public class Rue {
+import cityproject.infrastructures.geographie.Coordonnees4Points;
+import cityproject.infrastructures.geographie.DirectionEnum;
 
+public class Rue extends Infrastructure {
+	private Coordonnees4Points<?> coordonnees;
+	private DirectionEnum direction;
+	
 	public Rue() {
-		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public Rue (String nom) {
+		super(nom);
+	}
+	
+	public Rue (String nom, Coordonnees4Points<?> coordonnees) throws Exception {
+		setNom(nom);
+		setCoordonnees(coordonnees);
+	
+	}
+
+	public Coordonnees4Points<?> getCoordonnees() {
+		return coordonnees;
+	}
+
+	public void setCoordonnees(Coordonnees4Points<?> coordonnees2) throws Exception {
+		if (coordonnees2.isValid()) {
+			this.coordonnees = coordonnees2;
+		} else {
+			throw new Exception ("Ces coordonnees geographiques ne sont pas valides pour une ville");
+		}
+	}
+
+	public DirectionEnum getDirection() {
+		return direction;
+	}
+
+	public void setDirection(DirectionEnum direction) {
+		this.direction = direction;
 	}
 
 }
