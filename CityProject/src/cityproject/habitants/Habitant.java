473f0792551d9dd11af11c23aba7723d9bde0101
Yoann <Yoann@PC-Yoann-Bureau>
2017-04-03 17:40:24 +0200

    Test2

---
+++ CityProject/src/cityproject/habitants/Habitant.java
@@ -0,0 +1,15 @@
+package cityproject.habitants;
+
+import cityproject.infrastructures.Habitation;
+import cityproject.infrastructures.geographie.Point;
+
+public class Habitant {
+	private Point localisation;
+	public void rentrer(Habitation hab){
+		
+	}
+
+	public void marcher(){
+		
+	}
+}
---
+++ CityProject/src/cityproject/infrastructures/Rue.java
@@ -0,0 +1,20 @@
+package cityproject.infrastructures;
+
+public class Rue {
+	private String nom;
+	private int longueur;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getLongueur() {
+		return longueur;
+	}
+	public void setLongueur(int longueur) {
+		this.longueur = longueur;
+	}
+}
+
+	
