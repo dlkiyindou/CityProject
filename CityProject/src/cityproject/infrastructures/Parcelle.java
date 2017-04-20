539074b21bae89c6b201d599a64026661e1aa3f4
Yoann <yoann.farre@gmail.com>
2017-04-20 11:08:36 +0200

    Méthode calculCoordonneesParcelle(). Attention contient des erreurs car fait appel à des méthodes qui n'existent pas encore.

--- CityProject/src/cityproject/infrastructures/Parcelle.java
+++ CityProject/src/cityproject/infrastructures/Parcelle.java
@@ -1,6 +1,5 @@
 package cityproject.infrastructures;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +9,8 @@ import cityproject.infrastructures.geographie.PointKm;
 public class Parcelle {
 
 	private List<Croisement> croisements = new ArrayList<Croisement>();
-	private Coordonnees4Points coord_parcelle;
+	private List<Rue> rues = new ArrayList<Rue>();
+	private Coordonnees4Points<PointKm> coord_parcelle, coord_croisement;
 
 	/*
 	 * Constructeurs
@@ -20,7 +20,7 @@ public class Parcelle {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Parcelle(Coordonnees4Points coord) {
+	public Parcelle(Coordonnees4Points<PointKm> coord) {
 		this.coord_parcelle = coord;
 
 	}
@@ -50,36 +50,183 @@ public class Parcelle {
 		return croisements.contains(_croisement);
 	}
 
+	/*
+	 * Méthode de calcul des coordonnees de parcelle
+	 */
+
 	public void calculCoordonneesParcelle() {
-		
-		pointNordOuest
 
-		if (croisements.size() == 4) {
+		for (Croisement c : croisements) {
 
-			for (Croisement c : croisements) {
+			List<PointKm> coord_croisement = c.getCoordonneesCroisement();
 
-				List<PointKm> listeDePoints = c.getCoordonneesCroisement();
+			int i = 0;
+			PointKm point1 = new PointKm(0, 0); // point de parcours sur axe E-O
+			PointKm point2 = new PointKm(0, 0); // point de parcours sur axe N-S
+			PointKm point3 = new PointKm(0, 0); // dernier point pour constituer la parcelle, les coordonnees sont calculées
 
-				for (PointKm p : listeDePoints) {
+			for (PointKm p : coord_croisement) {
+
+				int x1, y1, x2, y2, x3, y3;
+
+				x1 = (int) p.getX();
+				y1 = (int) p.getY();
+				point1.setLocation(x1, y1);
+
+				x2 = (int) p.getX();
+				y2 = (int) p.getY();
+				point2.setLocation(x2, y2);
+				
+				/*
+				 * Premier cas: On part du point NO et on calcul les coordonnées
+				 * de la parcelle
+				 */
+
+				if (i == 0) {
+
+					do {
+						x1 -= 1;
+						point1.setLocation(x1, y1);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode isPointExtremeRue ou isPointCroissement
+
+					do {
+						y2 += 1;
+						point2.setLocation(x2, y2);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode à décrire dans Croisement ou Rue
 					
-					if (c.getPointIntersection().getX() > pointNordOuest.getX()){
-						
-					}
+					x3 = (int) point1.getX();
+					y3 = (int) point2.getY();
+					point3.setLocation(x3, y3);
+					
+					coord_parcelle.ajouterElement(point3); //point NO
+					coord_parcelle.ajouterElement(point2); //point NE
+					coord_parcelle.ajouterElement(point1); //point SO
+					coord_parcelle.ajouterElement(p);	//point SE
+					
+					i++;
+				}
+				
+				/*
+				 * Deuxième cas: On part du point NE et on calcul les coordonnées
+				 * de la parcelle
+				 */
 
+				if (i == 1) {
+
+					do {
+						x1 += 1;
+						point1.setLocation(x1, y1);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode isPointExtremeRue ou isPointCroissement
+
+					do {
+						y2 += 1;
+						point2.setLocation(x2, y2);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode à décrire dans Croisement ou Rue
+					
+					x3 = (int) point1.getX();
+					y3 = (int) point2.getY();
+					point3.setLocation(x3, y3);
+					
+					coord_parcelle.ajouterElement(point2);// point NO
+					coord_parcelle.ajouterElement(point3); //point NE
+					coord_parcelle.ajouterElement(p); //point SO
+					coord_parcelle.ajouterElement(point1); //point SE
+					
+					
+					i++;
+				}
+				
+				/*
+				 * Troisième cas: On part du point SO et on calcule les coordonnées
+				 * de la parcelle
+				 */
+
+				if (i == 2) {
+
+					do {
+						x1 -= 1;
+						point1.setLocation(x1, y1);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode isPointExtremeRue ou isPointCroissement
+
+					do {
+						y2 -= 1;
+						point2.setLocation(x2, y2);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode à décrire dans Croisement ou Rue
+					
+					x3 = (int) point1.getX();
+					y3 = (int) point2.getY();
+					point3.setLocation(x3, y3);
+					
+					coord_parcelle.ajouterElement(point1); //Point NO
+					coord_parcelle.ajouterElement(p); // Point NE
+					coord_parcelle.ajouterElement(point3); // Point SO
+					coord_parcelle.ajouterElement(point2); // Point SE
+					
+					
+					i++;
+				}
+				
+				/*
+				 * Quatrième cas: On part du point SE et on calcule les coordonnées
+				 * de la parcelle
+				 */
+
+				if (i == 3) {
+
+					do {
+						x1 += 1;
+						point1.setLocation(x1, y1);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode isPointExtremeRue ou isPointCroissement
+
+					do {
+						y2 -= 1;
+						point2.setLocation(x2, y2);
+					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+					// méthode à décrire dans Croisement ou Rue
+					
+					x3 = (int) point1.getX();
+					y3 = (int) point2.getY();
+					point3.setLocation(x3, y3);
+					
+					coord_parcelle.ajouterElement(p); //Point NO
+					coord_parcelle.ajouterElement(point1); // Point NE
+					coord_parcelle.ajouterElement(point2); // Point SO
+					coord_parcelle.ajouterElement(point3); // Point SE
 				}
 			}
+
 		}
 
-//		if (croisements.size() == 1) {
-//
-//		} else if (croisements.size() == 2) {
-//
-//		} else if (croisements.size() == 3) {
-//
-//		}
-	
+		// if (croisements.size() == 4) {
+		//
+		// for (Croisement c : croisements) {
+		//
+		// List<PointKm> listeDePoints = c.getCoordonneesCroisement();
+		//
+		// for (PointKm p : listeDePoints) {
+		//
+		// if (c.getPointIntersection().getX() > pointNordOuest.getX()){
+		//
+		// }
+		//
+		// }
+		// }
+		// }
 
-		
+		// if (croisements.size() == 1) {
+		//
+		// } else if (croisements.size() == 2) {
+		//
+		// } else if (croisements.size() == 3) {
+		//
+		// }
 
 	}
 }
