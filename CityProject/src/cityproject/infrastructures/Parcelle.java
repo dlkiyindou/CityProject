46300d2cee9166e4952d7e391999b16128f9e1ce
Yoann <yoann.farre@gmail.com>
2017-04-21 17:57:12 +0200

    Changement calculPositionParcelle pour l'adapter au calcul pour une seule parcelle (en cours, attention erreur car méthodes manquantes dans d'autres classes)

--- CityProject/src/cityproject/infrastructures/Parcelle.java
+++ CityProject/src/cityproject/infrastructures/Parcelle.java
@@ -8,9 +8,7 @@ import cityproject.infrastructures.geographie.PointKm;
 import cityproject.infrastructures.geographie.PointM;
 
 public class Parcelle {
-	
-	
-	
+
 	private Coordonnees4Points<PointM> coord_parcelle;
 
 	/*
@@ -26,209 +24,194 @@ public class Parcelle {
 
 	}
 
-public Coordonnees4Points<PointM> getCoordonneesParcelle(){
-	
-	return coord_parcelle;
-}
+	public Coordonnees4Points<PointM> getCoordonneesParcelle() {
+		return coord_parcelle;
+	}
 
 	/*
-	 * Méthode pour vérifier qu'un croisement est contenu dans la liste de
-	 * croisements
+	 * Méthode de calcul de la position d'une parcelle
 	 */
 
-//	public boolean asCroisement(Croisement _croisement) {
-//		
-//		List<Croisement> croisements = new ArrayList<Croisement>();
-//		
-//		return croisements.contains(_croisement);
-//	}
+	public void calculPositionParcelle(PointM p, Croisement c) {
 
-	/*
-	 * Méthode de calcul des positions de toutes les parcelles
-	 */
 
-	public void calculPositionsParcelles() {
-		
-		Ville ville = new Ville();
 		List<Parcelle> parcelles = new ArrayList<Parcelle>();
-		List<Croisement> croisements = new ArrayList<Croisement>();
-		List<Rue> rues = new ArrayList<Rue>();
 		
-		//3 méthodes à définir dans la Class Ville ou Quartier peut être pour récupérer les listes de parcelles, rues, et croisements
-		parcelles = ville.getListeDeParcelles();
-		croisements = ville.getListeDeCroisements();
-		//rues = ville.getListeDeRues();
+		// Ville ville = new Ville();
+		// List<Croisement> croisements = new ArrayList<Croisement>();
+		// List<Rue> rues = new ArrayList<Rue>();
 
-		for (Croisement c : croisements) {
+		// 3 méthodes à définir dans la Class Ville ou Quartier peut être pour
+		// récupérer les listes de parcelles, rues, et croisements
+		// parcelles = ville.getListeDeParcelles();
+		// croisements = ville.getListeDeCroisements();
+		// rues = ville.getListeDeRues();
 
-			List<PointM> coord_croisement = c.getCoordonneesCroisement();
+		Coordonnees4Points<PointM> coord_croisement = c.getCoordonneesCroisement();
 
-			int i = 0;
-			int x1, y1, x2, y2, x3, y3;
-			PointM point1 = null; // point de parcours sur axe E-O
-			PointM point2 = null; // point de parcours sur axe N-S
-			PointM point3 = null; // dernier point pour constituer la parcelle, les coordonnees sont calculées
+		int i = 0;
+		int x1, y1, x2, y2, x3, y3;
+		PointM point1 = null; // point de parcours sur axe E-O
+		PointM point2 = null; // point de parcours sur axe N-S
+		PointM point3 = null; // dernier point pour constituer la parcelle, les
+								// coordonnees sont calculées
+		
+		// Rajouter un appel à méthode pour déterminer la position d'un point en termes cardinaux NO NE SO SE dans la série de point croisement
+		// méthode à écrire dans point probablement
 
-			for (PointM p : coord_croisement) {
+		x1 = (int) p.getX();
+		y1 = (int) p.getY();
+		point1.setLocation(x1, y1);
 
-				x1 = (int) p.getX();
-				y1 = (int) p.getY();
+		x2 = (int) p.getX();
+		y2 = (int) p.getY();
+		point2.setLocation(x2, y2);
+
+		/*
+		 * Premier cas: On part du point NO et on calcule les coordonnées de la
+		 * parcelle
+		 */
+
+		if (i == 0) {
+
+			do {
+				x1 -= 1;
 				point1.setLocation(x1, y1);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode isPointExtremeRue ou isPointCroissement
 
-				x2 = (int) p.getX();
-				y2 = (int) p.getY();
+			do {
+				y2 += 1;
 				point2.setLocation(x2, y2);
-				
-				/*
-				 * Premier cas: On part du point NO et on calcul les coordonnées
-				 * de la parcelle
-				 */
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode à décrire dans Croisement ou Rue
 
-				if (i == 0) {
+			x3 = (int) point1.getX();
+			y3 = (int) point2.getY();
+			point3.setLocation(x3, y3);
 
-					do {
-						x1 -= 1;
-						point1.setLocation(x1, y1);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode isPointExtremeRue ou isPointCroissement
+			coord_parcelle.ajouterElement(point3); // point NO
+			coord_parcelle.ajouterElement(point2); // point NE
+			coord_parcelle.ajouterElement(point1); // point SO
+			coord_parcelle.ajouterElement(p); // point SE
 
-					do {
-						y2 += 1;
-						point2.setLocation(x2, y2);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode à décrire dans Croisement ou Rue
-					
-					x3 = (int) point1.getX();
-					y3 = (int) point2.getY();
-					point3.setLocation(x3, y3);
-					
-					coord_parcelle.ajouterElement(point3); //point NO
-					coord_parcelle.ajouterElement(point2); //point NE
-					coord_parcelle.ajouterElement(point1); //point SO
-					coord_parcelle.ajouterElement(p);	//point SE
-					
-					parcelles.add(this);
-					
-					i++;
-				}
-				
-				/*
-				 * Deuxième cas: On part du point NE et on calcule les coordonnées
-				 * de la parcelle
-				 */
+			parcelles.add(this);
 
-				if (i == 1) {
+			i++;
+		}
 
-					do {
-						x1 += 1;
-						point1.setLocation(x1, y1);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode isPointExtremeRue ou isPointCroissement
+		/*
+		 * Deuxième cas: On part du point NE et on calcule les coordonnées de la
+		 * parcelle
+		 */
 
-					do {
-						y2 += 1;
-						point2.setLocation(x2, y2);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode à décrire dans Croisement ou Rue
-					
-					x3 = (int) point1.getX();
-					y3 = (int) point2.getY();
-					point3.setLocation(x3, y3);
-					
-					coord_parcelle.ajouterElement(point2);// point NO
-					coord_parcelle.ajouterElement(point3); //point NE
-					coord_parcelle.ajouterElement(p); //point SO
-					coord_parcelle.ajouterElement(point1); //point SE
-					
-					parcelles.add(this);
-					
-					i++;
-				}
-				
-				/*
-				 * Troisième cas: On part du point SO et on calcule les coordonnées
-				 * de la parcelle
-				 */
+		if (i == 1) {
 
-				if (i == 2) {
+			do {
+				x1 += 1;
+				point1.setLocation(x1, y1);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode isPointExtremeRue ou isPointCroissement
 
-					do {
-						x1 -= 1;
-						point1.setLocation(x1, y1);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode isPointExtremeRue ou isPointCroissement
+			do {
+				y2 += 1;
+				point2.setLocation(x2, y2);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode à décrire dans Croisement ou Rue
 
-					do {
-						y2 -= 1;
-						point2.setLocation(x2, y2);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode à décrire dans Croisement ou Rue
-					
-					x3 = (int) point1.getX();
-					y3 = (int) point2.getY();
-					point3.setLocation(x3, y3);
-					
-					coord_parcelle.ajouterElement(point1); //Point NO
-					coord_parcelle.ajouterElement(p); // Point NE
-					coord_parcelle.ajouterElement(point3); // Point SO
-					coord_parcelle.ajouterElement(point2); // Point SE
-					
-					parcelles.add(this);
-					
-					i++;
-				}
-				
-				/*
-				 * Quatrième cas: On part du point SE et on calcule les coordonnées
-				 * de la parcelle
-				 */
+			x3 = (int) point1.getX();
+			y3 = (int) point2.getY();
+			point3.setLocation(x3, y3);
 
-				if (i == 3) {
+			coord_parcelle.ajouterElement(point2);// point NO
+			coord_parcelle.ajouterElement(point3); // point NE
+			coord_parcelle.ajouterElement(p); // point SO
+			coord_parcelle.ajouterElement(point1); // point SE
 
-					do {
-						x1 += 1;
-						point1.setLocation(x1, y1);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode isPointExtremeRue ou isPointCroissement
+			parcelles.add(this);
 
-					do {
-						y2 -= 1;
-						point2.setLocation(x2, y2);
-					} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
-					// méthode à décrire dans Croisement ou Rue
-					
-					x3 = (int) point1.getX();
-					y3 = (int) point2.getY();
-					point3.setLocation(x3, y3);
-					
-					coord_parcelle.ajouterElement(p); //Point NO
-					coord_parcelle.ajouterElement(point1); // Point NE
-					coord_parcelle.ajouterElement(point2); // Point SO
-					coord_parcelle.ajouterElement(point3); // Point SE
-					
-					parcelles.add(this);
-					
-				}
-			}
+			i++;
+		}
+
+		/*
+		 * Troisième cas: On part du point SO et on calcule les coordonnées de
+		 * la parcelle
+		 */
+
+		if (i == 2) {
+
+			do {
+				x1 -= 1;
+				point1.setLocation(x1, y1);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode isPointExtremeRue ou isPointCroissement
+
+			do {
+				y2 -= 1;
+				point2.setLocation(x2, y2);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode à décrire dans Croisement ou Rue
+
+			x3 = (int) point1.getX();
+			y3 = (int) point2.getY();
+			point3.setLocation(x3, y3);
+
+			coord_parcelle.ajouterElement(point1); // Point NO
+			coord_parcelle.ajouterElement(p); // Point NE
+			coord_parcelle.ajouterElement(point3); // Point SO
+			coord_parcelle.ajouterElement(point2); // Point SE
+
+			parcelles.add(this);
+
+			i++;
+		}
+
+		/*
+		 * Quatrième cas: On part du point SE et on calcule les coordonnées de
+		 * la parcelle
+		 */
+
+		if (i == 3) {
+
+			do {
+				x1 += 1;
+				point1.setLocation(x1, y1);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode isPointExtremeRue ou isPointCroissement
+
+			do {
+				y2 -= 1;
+				point2.setLocation(x2, y2);
+			} while ((point1 != p.isPointExtremeRue() || point1 != p.isPointCroisement));
+			// méthode à décrire dans Croisement ou Rue
+
+			x3 = (int) point1.getX();
+			y3 = (int) point2.getY();
+			point3.setLocation(x3, y3);
+
+			coord_parcelle.ajouterElement(p); // Point NO
+			coord_parcelle.ajouterElement(point1); // Point NE
+			coord_parcelle.ajouterElement(point2); // Point SO
+			coord_parcelle.ajouterElement(point3); // Point SE
+
+			parcelles.add(this);
 
 		}
 
 	}
-	
-	//Méthode de mise à jour de la liste de parcelle avec élimination des doublons et des parcelles chevauchantes.
-	
-//	public void simplifyListeDeParcelle() {
-//		
-//		Ville ville = new Ville();
-//		List<Parcelle> parcelles = new ArrayList<Parcelle>();
-//		
-//		parcelles = ville.getListeDeParcelles();
-//		
-//		for (Parcelle p: parcelles){
-//			
-//			
-//		}
-//		
-//	}
-	
+
+	// Méthode de mise à jour de la liste de parcelle avec élimination des
+	// doublons et des parcelles chevauchantes.
+
+	// public void simplifyListeDeParcelle() {
+	//
+	// Ville ville = new Ville();
+	// List<Parcelle> parcelles = new ArrayList<Parcelle>();
+	//
+	// parcelles = ville.getListeDeParcelles();
+	//
+	// for (Parcelle p: parcelles){
+	// }
+	// }
+
 }
