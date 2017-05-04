cc3d5331d33bf8f500f82e3bbc05d7323cbd31f2
James <james.ryder@hotmail.com>
2017-05-04 14:35:10 +0200

    développement de croisement algorithme

--- CityProject/src/cityproject/infrastructures/Croisement.java
+++ CityProject/src/cityproject/infrastructures/Croisement.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import cityproject.infrastructures.geographie.Coordonnees4Points;
-import cityproject.infrastructures.geographie.PointKm;
 import cityproject.infrastructures.geographie.PointM;
+
 
 public class Croisement extends Infrastructure {
 	private List<Rue> rues;
-
+	
 	public Croisement() {
 		super();
 		rues = new ArrayList<Rue>();
@@ -51,6 +51,8 @@ public class Croisement extends Infrastructure {
 		return null;
 	}
 	
+	public static void main(String[] args){
+
 	
 // Pseudo-code (James R)
 	
@@ -58,39 +60,96 @@ public class Croisement extends Infrastructure {
 //	
 //  1) liste des points d'Origine de chaque rue (pointOrigine: 
 //	        PointKm cityproject.infrastructures.Rue.pointOrigine))	
-//          orig_x, orig_y
+//          orig_x_1, orig_y_1, orig_x_2, orig_y_2
+//
+double orig_x_1 = 2; double orig_y_1 = 2;
+//
+double orig_x_2 = 4; double orig_y_2 = 4;
 //
 //  2) liste du direction de chaque rue (direction - 
 //	        cityproject.infrastructures.Rue.setDirection(DirectionEnum))	
 //          m_1, n_1, m_2, n_2
 //
+//  IF direction = HORIZONTAL, m=0; n=1
+//  IF direction = VERTICAL, m=1; n=0
+//
+double m_1 = 0;
+double n_1 = 1;
+double m_2 = 1;
+double n_2 = 0;
+//
 //  3) liste du longueur de chaque rue (int longueur - 
 //       	cityproject.infrastructures.Rue.setLongueur(int))
+//
+double longeur_1 = 10;
+double longeur_2 = 20;
 //
 //  4) liste du largeur de chaque rue (largeur - 
 //	        cityproject.infrastructures.Rue.largeur))	
 //
+double largeur_1 = 0.5;
+double largeur_2 = 0.5;
+
+System.out.print("largeur_1: " +largeur_1+ ",   ");
+System.out.println("largeur_2: " +largeur_2+ ",   ");
+
+
+//
 //  5) créer une équation pour chaque rue
-//     e.g. n_1*y = m_1(x - orig_x_1) + orig_y_1
-//          n_2*y = m_2(x - orig_x_2) + orig_y_2
-	
-//     IF direction = HORIZONTAL, m=0; n=1
-//     IF direction = VERTICAL, m=1; n=0
+//     e.g. i)  n_1*(y - orig_y_1) = m_1(x - orig_x_1)
+//          ii) n_2*(y - orig_y_2) = m_2(x - orig_x_2)
+//
+//  IF direction = HORIZONTAL +ve, m=0; n=1
+//  IF direction = HORIZONTAL -ve, m=0; n=-1
+//
+//  IF direction = VERTICAL +ve, m=1; n=0
+//  IF direction = VERTICAL -ve, m=-1; n=0
 //
 //  6) reorganiser chaque équation sous la forme:
 //     (avec les nouvelles variables A_1, B_1, C_1, A_2, B_2, C_2)	
-//     i) A_1*x + B_1*y = C_1	
+//     i)  A_1*x + B_1*y = C_1	
 //     ii) A_2*x + B_2*y = C_2
+//
+//     i)  (m_1 * x) - (n_1 * y) = (m_1 * orig_x_1) - (n_1 * orig_y_1)
+//     ii) (m_2 * x) - (n_2 * y) = (m_2 * orig_x_2) - (n_2 * orig_y_2)
+//
+double A_1 = m_1;
+double B_1 = -n_1;
+double C_1 = (m_1 * orig_x_1) - (n_1 * orig_y_1);
+double A_2 = m_2;
+double B_2 = -n_2;
+double C_2 = (m_2 * orig_x_2) - (m_1 * orig_y_2);
+
+
+System.out.print("A_1: " +A_1+ ",   ");
+System.out.print("B_1: " +B_1+ ",   ");
+System.out.println("C_1: " +C_1+ ",   ");
+System.out.print("A_2: " +A_2+ ",   ");
+System.out.print("B_2: " +B_2+ ",   ");
+System.out.println("C_2: " +C_2+ ",   ");
+
 //
 //  7) definir:
 //     delta = A_1*B_2 - A_2*B_1
+//
+double delta = ((A_1*B_2) - (A_2*B_1));
+//
+System.out.println("delta: " +delta+ ",   ");
+//
 //
 //  8) IF: delta = 0
 //        aucun intercept
 //  
 //  9) ELSE: les points d'intercept sont x_cross et y_cross:
-//        X_cross = (B_2*C_1 - B_1*C_2)/delta
-//        Y_cross = (A_1*C_2 - A_2*C_1)/delta
+//        x_cross = (B_2*C_1 - B_1*C_2)/delta
+//        y_cross = (A_1*C_2 - A_2*C_1)/delta
+//
+double x_cross=((B_2*C_1) - (B_1*C_2))/delta;	
+double y_cross=((A_1*C_2) - (A_2*C_1))/delta;
+//
+
+//
+//
 //
 //  10) vérifier si ç'est bien un intercept, quand on prend
 //        compte du longeur de la rue
@@ -99,6 +158,23 @@ public class Croisement extends Infrastructure {
 //         aucun intercept
 //      ELSE
 //         continue	
+
+if ((Math.pow((x_cross - orig_x_1),2) + Math.pow((y_cross - orig_y_1),2)) >= Math.pow(longeur_1,2)) {
+	System.out.println("rue 1 trop court");
+} else if ((Math.pow((x_cross - orig_x_1),2) + Math.pow((y_cross - orig_y_1),2)) >= Math.pow(longeur_2,2)) {
+	System.out.println("rue 2 trop court");
+} else if (delta == 0) {
+	System.out.println("rue 2 trop court");
+} 
+  else {
+	System.out.println("c'est bien un croisement");
+	System.out.println("    ");
+	System.out.println("Coordonnés de croisement");
+	System.out.println("------------------------");
+
+	System.out.print("x_cross: " +x_cross+ ",   ");
+	System.out.println("y_cross: " +y_cross+ ",   ");
+}
 //
 //  11) désigner les 'coins' de l'interception
 //        x1 = x_cross + (largueur_1 / 2)	
@@ -110,7 +186,36 @@ public class Croisement extends Infrastructure {
 //	      y2 = y_cross + (largueur_1 / 2)
 //        y3 = y_cross + (largueur_2 / 2)	
 //        y4 = y_cross + (largueur_2 / 2)
-//	
+//
+double x1 = x_cross + (largeur_1 / 2);	
+double x2 = x_cross - (largeur_1 / 2);
+double x3 = x_cross + (largeur_2 / 2);	
+double x4 = x_cross - (largeur_2 / 2);
+//
+double y1 = y_cross + (largeur_1 / 2);	
+double y2 = y_cross + (largeur_1 / 2);
+double y3 = y_cross + (largeur_2 / 2);	
+double y4 = y_cross + (largeur_2 / 2);
+//
+
+System.out.println("    ");
+System.out.println("les coins de chaque croisement");
+System.out.println("------------------------------");
+
+
+System.out.print("x1: "+x1+ ", ");
+System.out.print("x2: "+x2+ ", ");
+System.out.print("x3: "+x3+ ", ");
+System.out.println("x4: "+x4+ ", ");
+
+System.out.print("y1: "+y1+ ", ");
+System.out.print("y2: "+y2+ ", ");
+System.out.print("y3: "+y3+ ", ");
+System.out.println("y4: "+y4+ ", ");
+
+}
+
+//
 //	public Coordonnees4Points getCoordonneesCroisement() {
 //		
 //		return coord;
