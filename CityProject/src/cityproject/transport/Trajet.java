48c8ee9da2cc6afebf3ab4f24036fe0516a1b5ac
sego <sego.maris@gmail.com>
2017-05-18 17:56:02 +0200

    ajout modif Cecilia

--- CityProject/src/cityproject/transport/Trajet.java
+++ CityProject/src/cityproject/transport/Trajet.java
@@ -10,9 +10,13 @@ import cityproject.infrastructures.geographie.DirectionEnum;
 
 public class Trajet {
 	
-	private CoordonneesGeographiques<PointM> coordTrajet = new CoordonneesGeographiques<PointM>();
+	private CoordonneesGeographiques <PointM> coordTrajet = new CoordonneesGeographiques<PointM>();
+	PointM point_reference_origine1 = new PointM();
+	PointM point_reference_origine2 = new PointM();
+	PointM point_reference_fin1 = new PointM();
+	PointM point_reference_fin2 = new PointM();
 	
-	//Contructeur par defaut 
+	//Constructeur par défaut 
 	public Trajet() throws CityProjectException {
 		setCoordTrajet(null);
 	}
@@ -28,117 +32,157 @@ public class Trajet {
 	}
 	
 	
-	
 	//Setter
 	public void setCoordTrajet(CoordonneesGeographiques<PointM> coordTrajet) {
 		this.coordTrajet = coordTrajet;
 	}
 	
-	//Methode de calcul de trajet
-	public void calculTrajet (PointM pointOrigin, PointM pointFinal) throws CityProjectException {
+	//Méthode de calcul du trajet
+	public void calculTrajet (PointM point_debut, PointM point_fin) throws CityProjectException {
 	
-		//premier point du trajet
-		coordTrajet.ajouterElement(pointOrigin);
 		
-		//po est dans une rue 
-		Rue rueO = new Rue ();
-		DirectionEnum direction= rueO.getDirection();
+		//ajouter le premier point du trajet, po, à coordTrajet
+		coordTrajet.ajouterElement(point_debut);
+		
+		//Les points origine et final sont dans une rue: il faudra obtenir la rue à partir de chaque point (créer méthode) 
+		Rue rue_origine = new Rue (); //à changer en utilisant la méthode à créer
+		Rue rue_final = new Rue (); //à changer en utilisant la méthode à créer 
+		DirectionEnum direction_origine= rue_origine.getDirection();
+		DirectionEnum direction_final = rue_final.getDirection();
 		
 		
-		//OrdonnÃ©es et abscises des points origine et final du trajet
-		int xo=(int)pointOrigin.getX();
-		int yo=(int)pointOrigin.getY();
+		//Ordonnées et abscisses des points origine et final du trajet
+		int x_debut=(int)point_debut.getX();
+		int y_debut=(int)point_debut.getY();
 		
-		int xf=(int)pointFinal.getX();
-		int yf=(int)pointFinal.getY();
+		int x_fin=(int)point_fin.getX();
+		int y_fin=(int)point_fin.getY();
 		
 		//coordonnees temporaux
-		int xt, yt;
-		PointM pt= new PointM(0,0);
+		int x_temporal, y_temporal;
+		PointM point_parcours= new PointM(0,0);
 		
-		while((pointOrigin != pointFinal) && (pt != pointFinal)){
-			
+		// Cas dans lequel le point debut et fin sont dans le même "morceau" de rue (entre le deux même croisement)!!
 		
-			//Si po est dans une rue vertical
-			if (direction==DirectionEnum.VERTICAL) {
-			
-			
-				if (yo>yf) {
-					//Balayer sur y jusqu'au croisement plus proche entre po et pf
-					yt=yo;
-				
-					do{ 
-						yt--;
-						pt.setLocation(xo, yt);
-					}while ((pt.isPointCroisement() != null) && (yt != yf));
-				
-					//Ajouter le point du croisement au trajet
-					coordTrajet.ajouterElement(pt);
-				}
-			
-				if (yo<yf) {
-					//Balayer sur y jusqu'au croisement plus proche entre po et pf
-					yt=yo;
-				
-					do{ 
-						yt++;
-						pt.setLocation(xo, yt);
-					}while ((pt.isPointCroisement() != null) && (yt != yf));
-				
-					//Ajouter le point du croisement au trajet
-					coordTrajet.ajouterElement(pt);
-				
-				}
-			
-			
-			
-				//Tester encore sur y pour savoir s'il faut continuer
-			
-			
-			}
 		
-			//Si po est dans une rue vertical
-			if (direction==DirectionEnum.HORIZONTAL) {
+		//Détermination de points de référence pour le point origine
+		
+		if (direction_origine == DirectionEnum.HORIZONTAL) {
 			
-				//Balayer sur x jusqu'au croisement plus proche entre po et pf
-				if (xo>xf) {
-					
-					//Balayer sur x jusqu'au croisement plus proche entre po et pf
-					xt=xo;
-					
-					do{ 
-						xt--;
-						pt.setLocation(xt, yo);
-					}while ((pt.isPointCroisement() != null) && (xt != xf));
-					
-					//Ajouter le point du croisement au trajet
-					coordTrajet.ajouterElement(pt);
-				}
-				
-				if (xo<xf) {
-					
-					//Balayer sur x jusqu'au croisement plus proche entre po et pf
-					xt=xo;
-					
-					do{ 
-						xt++;
-						pt.setLocation(xt, yo);
-					}while ((pt.isPointCroisement() != null) && (xt != xf));
-					
-					//Ajouter le point du croisement au trajet
-					coordTrajet.ajouterElement(pt);
-					
-				}
-				
-			}
+			//Point référence origine 1: balayer sur x jusqu'au croisement plus proche à gauche
 			
-			//point final du trajet
-			coordTrajet.ajouterElement(pointFinal);
+			x_temporal=x_debut;
+		
+			do{ 
+				x_temporal--;
+				point_parcours.setLocation(x_temporal, y_debut);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_origine1 = point_parcours;
+						
+			//Point référence origine 2: balayer sur x jusqu'au croisement plus proche à droite
+			
+			x_temporal=x_debut;
+		
+			do{ 
+				x_temporal++;
+				point_parcours.setLocation(x_temporal, y_debut);
+			}while (point_parcours.isPointCroisement() != null) ;
+			
+			point_reference_origine2 = point_parcours;
 			
 			
 		}
 		
 		
+		if (direction_origine == DirectionEnum.VERTICAL) {
+			
+			//Point référence origine 1: Balayer sur y jusqu'au croisement plus proche au-dessus
+			
+			y_temporal=y_debut;
+		
+			do{ 
+				y_temporal--;
+				point_parcours.setLocation(x_debut, y_temporal);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_origine1 = point_parcours;
+			
+			
+			//Point référence origine 2: balayer sur y jusqu'au croisement plus proche en dessous
+			
+			y_temporal=y_debut;
+		
+			do{ 
+				y_temporal++;
+				point_parcours.setLocation(x_debut, y_temporal);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_origine2 = point_parcours;
+			
+			
+		}
+		
+		
+		//Détermination de points de référence pour le point final
+		
+		if (direction_final == DirectionEnum.HORIZONTAL) {
+			
+			//Point référence fin 1: balayer sur x jusqu'au croisement plus proche à gauche
+			
+			x_temporal=x_fin;
+		
+			do{ 
+				x_temporal--;
+				point_parcours.setLocation(x_temporal, y_fin);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_fin1 = point_parcours;
+						
+			//Point référence fin 2: balayer sur x jusqu'au croisement plus proche à droite
+			
+			x_temporal=x_fin;
+		
+			do{ 
+				x_temporal++;
+				point_parcours.setLocation(x_temporal, y_fin);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_fin2 = point_parcours;
+			
+			
+		}
+		
+		
+		if (direction_origine == DirectionEnum.VERTICAL) {
+			
+			//Point référence fin 1: Balayer sur y jusqu'au croisement plus proche au-dessus
+			
+			y_temporal=y_fin;
+		
+			do{ 
+				y_temporal--;
+				point_parcours.setLocation(x_fin, y_temporal);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_fin1 = point_parcours;
+			
+			
+			//Point référence origine 2: balayer sur y jusqu'au croisement plus proche en dessous
+			
+			y_temporal=y_fin;
+		
+			do{ 
+				y_temporal++;
+				point_parcours.setLocation(x_fin, y_temporal);
+			}while (point_parcours.isPointCroisement() != null);
+			
+			point_reference_fin2 = point_parcours;
+			
+			
+		}
+		
+	
 	}
 
 	
